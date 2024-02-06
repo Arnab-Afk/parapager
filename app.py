f4d266d5d9ec2a3d78ebfd324c972b5660b7776b
Arnab Bhowmik <arnabbhowmik019@gmail.com>
2024-02-06 23:30:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,10 @@ from langchain.prompts import PromptTemplate
 from langchain_community.document_loaders import WebBaseLoader
 from langchain.schema import StrOutputParser
 from langchain.schema.prompt_template import format_document
+from flask_cors import CORS
 import os   
 app = Flask(__name__)
+CORS(app) 
 os.environ["GOOGLE_API_KEY"] = "AIzaSyA4Kyt5llmYRxDkXfJ5yEX5iRu6wiBNgbM"
 
 from langchain_google_genai import ChatGoogleGenerativeAI
